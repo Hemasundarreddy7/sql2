77b9f7f2da9dbe82830d358793397f4568b4f4c2
Hemasundarreddy7 <140962374+Hemasundarreddy7@users.noreply.github.com>
2023-08-09 20:01:59 +0530

    Add files via upload

---
+++ day1 python.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name="hem"
+name="josh"
+print(name)
+
+
+# In[2]:
+
+
+n1="hii"
+n2="hello"
+print(n1)
+print(n2)
+
+
+# In[3]:
+
+
+kit age=chris age= prince=24
+print("kit age=",kit age);
+print("prince age=",prince age);
+
+
+# In[6]:
+
+
+kit_age=chris_age= prince_age=24
+print("kit age=",kit_age);
+print("prince age=",prince_age);
+
+
+# In[7]:
+
+
+name,age,sal="harsha",25,60000
+print("name=\n","age=\n","salary=",name,age,sal)
+
+
+# In[8]:
+
+
+name,age,sal="harsha",25,60000
+print("name="name)
+print("salary"=sal)
+print("age=",age)
+
+
+# # data type convertion
+
+# In[17]:
+
+
+price=5.6
+quantity=5
+totalprice=0
+totalprice=price*quantity
+print(totalprice)
+type(totalprice)
+
+
+# In[10]:
+
+
+#explicit conversion
+price=25
+tax="4.6"
+totalprice=price+float(tax)
+print("totalprice=",totalprice)
+
+
+# In[19]:
+
+
+marks=50.5
+print(str(marks))
+pas,fail=1,0
+print(bool(pas))
+print(bool(fail))
+
+
+# In[18]:
+
+
+a="5.4"
+int(a)
+
+
+# # print statements
+
+# In[21]:
+
+
+print("hii "+"how r u")
+
+
+# In[24]:
+
+
+print("hii","how r u",sep=",")
+
+
+# In[26]:
+
+
+print("hii","how r u")
+
+
+# In[28]:
+
+
+print("salary= "+25)
+
+
+# In[29]:
+
+
+print("salary= "+str(25))
+
+
+# # arithmetic operation
+
+# In[30]:
+
+
+price=10
+tax=0.5
+quantity=5
+total=(price+tax)*quantity
+print(total)
+
+
+# In[38]:
+
+
+a=" hii"
+n=4
+result=a*n
+print(result)
+
+
+# In[40]:
+
+
+side=10
+print("area of square=",side**2)
+print("area of cube=",side**3)
+
+
+# In[ ]:
+
+
+
+
